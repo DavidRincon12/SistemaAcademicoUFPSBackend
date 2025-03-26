07c68f58590cf75fe2ac16eaa1caed52bea2d46a
David Rincon <davidsantiagorb@ufps.edu.co>
2025-03-26 17:43:16 -0500

    Correccion Nombre

--- src/main/java/co/edu/ufps/SistemaAcademicoUFPSBackend/model/RecursoAcademico.java
+++ src/main/java/co/edu/ufps/SistemaAcademicoUFPSBackend/model/RecursoAcademico.java
@@ -19,7 +19,7 @@ public class RecursoAcademico implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
-    private String UbicacionRecurso;
+    private String ubicacionRecurso;
 
 
     @OneToOne(cascade = CascadeType.ALL)
