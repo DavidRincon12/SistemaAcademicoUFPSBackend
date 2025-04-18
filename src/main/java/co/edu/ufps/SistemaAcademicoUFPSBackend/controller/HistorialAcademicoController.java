b414b56056f2dbc35f0e1afd96c7e504b8c9e446
Linda Bohorquez <96446503+LindaBohorquez@users.noreply.github.com>
2025-04-18 15:55:22 -0500

    controller historial academico

--- src/main/java/co/edu/ufps/SistemaAcademicoUFPSBackend/controller/HistorialAcademicoController.java
+++ src/main/java/co/edu/ufps/SistemaAcademicoUFPSBackend/controller/HistorialAcademicoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/historiales")
+@RequestMapping("/historiales")
 @CrossOrigin(origins = "*")
 public class HistorialAcademicoController {
 
