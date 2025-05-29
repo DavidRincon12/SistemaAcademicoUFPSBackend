8249670d3242da490d57e9bc84b140ca7c4178b2
Linda Bohorquez <96446503+LindaBohorquez@users.noreply.github.com>
2025-05-29 18:34:03 -0500

    Docente

---
+++ src/main/java/co/edu/ufps/SistemaAcademicoUFPSBackend/DTO/DocenteDTO.java
@@ -0,0 +1,21 @@
+package co.edu.ufps.SistemaAcademicoUFPSBackend.DTO;
+
+import co.edu.ufps.SistemaAcademicoUFPSBackend.model.Docente;
+import lombok.Data;
+
+@Data
+public class DocenteDTO {
+    private Long id;
+    private String correoInstitucional;
+    private String tipo;
+    private Short cargaHoraria;
+    private Long personaId;
+
+    public DocenteDTO(Docente docente) {
+        this.id = docente.getId();
+        this.correoInstitucional = docente.getCorreoInstitucional();
+        this.tipo = docente.getTipo();
+        this.cargaHoraria = docente.getCargaHoraria();
+        this.personaId = docente.getPersona() != null ? docente.getPersona().getId() : null;
+    }
+}
